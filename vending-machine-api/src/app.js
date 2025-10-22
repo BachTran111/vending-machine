25168c77d1565bb733468caa1a784b7fec63f9ef
BachTran111 <tranxuanbach1029@gmail.com>
2025-10-22 10:14:32 +0700

    deploy testing

--- vending-machine-api/src/app.js
+++ vending-machine-api/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import path from "path";
 
 import productRouter from "./routes/product.route.js";
 import slotRouter from "./routes/slot.route.js";
@@ -9,6 +10,7 @@ import { errorHandler } from "./middlewares/error-handler.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
@@ -19,7 +21,32 @@ app.use("/api/products", productRouter);
 app.use("/api/slots", slotRouter);
 app.use("/api/transactions", transactionRouter);
 
-app.get("/", (req, res) => res.send("🚀 Vending Machine API running..."));
+// Serve static files
+app.use(express.static(path.join(__dirname, "../vending-machine-fe")));
+
+// Handle frontend routing
+app.get(
+  [
+    "/",
+    "/drink",
+    "/home",
+    "/report",
+    "/login",
+    "/register",
+    "/account",
+    "/forgotpassword",
+    "/t1",
+    "/t2",
+    "/t3",
+    "/t4",
+    "/t5",
+  ],
+  (req, res) => {
+    const page =
+      req.path === "/" ? "/index.html" : `${req.path.toLowerCase()}.html`;
+    res.sendFile(path.join(__dirname, "../vending-machine-fe", page));
+  }
+);
 
 // error handler (cuối cùng)
 app.use(errorHandler);
